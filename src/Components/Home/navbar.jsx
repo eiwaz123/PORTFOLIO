11297175dbcbc2cd4a02128511eb61345d69cd6b
eiwaz123 <pandrewroi@gmail.com>
2024-06-28 23:25:51 +0800

    Addfolder

--- src/Components/Home/navbar.jsx
+++ src/Components/Home/navbar.jsx
@@ -2,15 +2,14 @@ import react from "react";
 
 const Navbar = () => {
   return (
-    <nav  className="bg-black w-full px-1 py-4 flex items-center justify-evenly  transition-all delay-50 ease-in    " >
+    <nav  className="bg-black w-full px-4 py-4 flex items-center justify-evenly  transition-all delay-50 ease-in-out    " >
       <div className="text-white font-bold text-3xl  ">ROI PORTFOLIO</div>
       <div className="hidden items-center gap-4 text-1xl   md:flex     ">
-        <a className="text-white transition-all delay-50 ease-in  " href="#home"   >Home</a>
-        <a className="text-white transition-all delay-50 ease-in  " href="#about">About</a>
-        <a className="text-white transition-all delay-50 ease-in   " href="#">Contact</a>
-        <a className="text-white transition-all delay-50 ease-in  " href="#">Blog</a>
+        <a className="text-white transition-all delay-50 ease-in-out  " href="#home"   >Home</a>
+        <a className="text-white " href="#about">About</a>
+        <a className="text-white" href="#">Contact</a>
+        <a className="text-white" href="#">Blog</a>
         <button className=" bg-white px-4 py-2 rounded-full text-black font-bold ">Login</button>
-        <a className="text-white" href="#">TITE</a>
       </div>
 
     </nav>
